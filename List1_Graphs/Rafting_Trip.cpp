7d2261d52537b0b7602b61f3eaf4d64ce687c1bf
joao10guilherme <jbrunicataneodasilv@floridapoly.edu>
2026-09-16 15:29:03 -0400

    Templated added to Rafting_Trip solution.

---
+++ List1_Graphs/Rafting_Trip.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define per(i,a,b) for(int i=(b)-1;i>=(a);i--)
+#define pb push_back
+#define F first
+#define S second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
